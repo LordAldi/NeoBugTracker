1d3e43e176cda307a4dec0c598c12c230cb38d7a
LordAldi <aldianugra09@gmail.com>
2021-01-16 01:32:20 +0700

    setup express + typescript

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { indexRouter } from "./routes";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(indexRouter);
+
+export { app };
---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+require("dotenv").config();
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (!process.env.PORT) {
+    throw new Error("PORT must be defined");
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}!!!`);
+  });
+};
+
+start();
